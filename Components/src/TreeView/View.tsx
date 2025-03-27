88082438dd1c4f5cf85dab881b45db6888f5b7dd
sanjitkumar28 <sanjit2806@gmail.com>
2025-03-27 23:20:54 +0530

    completed functionality

--- Components/src/TreeView/View.tsx
+++ Components/src/TreeView/View.tsx
@@ -2,22 +2,22 @@ import { useState } from "react";
 
 export default function View({ data }: { data: any }) {
     const [expanded,setExpanded] = useState({
-        
-    } as any);
+
+    });
     console.log("Expanded",expanded);
   return (
     <div>
       {data.map((node: any, index: number) => {
         return (
           <div id="container" key={index}>
-            {node.children && <span onClick={()=>setExpanded((prev: any)=>({...prev,[node.label]:true}))}>+</span>}
+            {node.children && <span onClick={()=>setExpanded((prev: any)=>({...prev,[node.label]:!prev[node.label]}))}>{expanded[node.label]?"-":"+"}</span>}
             <span>{node.label}</span>
             {
                 node.children && expanded[node.label] && <View data={node.children}/>
             }
           </div>
         );
-      })}
+      })} 
     </div>
   );
 }
