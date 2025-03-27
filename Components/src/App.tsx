710759d2f88590108872c81b0c4caf7c07cf3cb7
sanjitkumar28 <sanjit2806@gmail.com>
2025-03-27 23:23:23 +0530

    renamed folders

--- Components/src/App.tsx
+++ Components/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import FileExproler from './TreeView/FileExproler'
+import FileExproler from './FileExproler/FileExproler'
 
 function App() {
  
