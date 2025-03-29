1772b82bc1b0282e289b8143fbe45af5d89d411d
sanjitkumar28 <sanjit2806@gmail.com>
2025-03-29 10:49:42 +0530

    added custom tabs

--- Components/src/Tabs/Tabs.tsx
+++ Components/src/Tabs/Tabs.tsx
@@ -1,9 +1,51 @@
-const Tabs=()=>{
-  return(
-    <div >
-        Tabs
+import { useState } from "react";
+import "./Tab.css";
+const Tabs = () => {
+  const tabs = [
+    {
+      label: "Tab1",
+      index: 0,
+      component: <div>This is Tab1</div>,
+    },
+    {
+      label: "Tab2",
+      index: 1,
+      component: <div>This is Tab2 </div>,
+    },
+    {
+      label: "Tab3",
+      index: 2,
+      component: <div>This is Tab 3</div>,
+    },
+  ];
+  const [tabIndex, setTabIndex] = useState(0);
+  const changeTabIndex = (event: any) => {
+    console.log("event", event);
+    setTabIndex(event);
+  };
+  return (
+    <>
+    <div className="tab-container">
+      {tabs.map((tab) => {
+        return (
+          <>
+            <div
+              className="tab"
+              key={tab.index}
+              onClick={() => changeTabIndex(tab.index)}
+            >
+              <span>{tab.label}</span>
+            </div>
+
+          </>
+        );
+      })}   
     </div>
-  )
-}
+      <div className="tabs-content">
+      {tabs[tabIndex]&& tabs[tabIndex].component}
+      </div>
+    </>
+  );
+};
 
 export default Tabs;
