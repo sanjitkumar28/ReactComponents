5f0a5529c49926416c20e1b9349b05c0ca40980c
sanjitkumar28 <sanjit2806@gmail.com>
2025-03-30 13:10:34 +0530

    implemented caching

--- Components/src/AutoComplete/AutoComplete.tsx
+++ Components/src/AutoComplete/AutoComplete.tsx
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios"
 import "./AutoComplete.styles.css"
+// Used Hashmap for caching or localStorage can be used
 const AutoComplete=()=>{
     const [input,setInput]=useState('');
     const [result,setResult]=useState([]);
+    const [showResults,setShowResults] = useState(false);
+    const [cacheResult,setCacheResult]=useState({})
+    console.log("cacheResult",cacheResult);
+
     const fetchData=async ()=>{
+    console.log("cache[input",cacheResult[input])
+    if(cacheResult[input]){
+        console.log("cache");
+        return cacheResult[input];
+    }
     const response= await axios.get("https://dummyjson.com/recipes/search?q="+input);
     setResult(response.data.recipes as any);
+    setCacheResult((prev)=>({...prev,[input]:response?.data?.recipes}))
     }
     
     useEffect(()=>{
@@ -21,10 +32,10 @@ const AutoComplete=()=>{
         <div className="container">
             <h1>AutoComplete</h1>
             <div>
-            <input value={input} onChange={(e)=>setInput(e.target.value)} style={{width:"500px"}}></input>
+            <input value={input} onChange={(e)=>setInput(e.target.value)} style={{width:"500px"}} onFocus={()=>setShowResults(true)} onBlur={()=>setShowResults(false)}></input>
             <div className="dropdown-container">
                {
-                result && result.map((data:any)=>{
+                showResults && result && result.map((data:any)=>{
                     return(
                           <div key={data.id} className="dropdown-item" >
                               {data.name}
